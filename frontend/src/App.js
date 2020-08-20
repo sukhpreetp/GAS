321ef5c85fcc035f761eabe4e36c4c7aadbc464a
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2020-08-20 23:22:09 +1000

    Implemented React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {getApi} from "./Api";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getApi } from "./Api";
 
 class App extends React.Component {
-    state = {
-        users: [],
-    }
+  state = {
+    users: [],
+  };
 
-    componentDidMount() {
-        getApi('/users').then(data => this.setState({users: data}));
-    }
+  componentDidMount() {
+    getApi("/users").then((data) => this.setState({ users: data }));
+  }
 
-    render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<p>
-						Users get from backend api and the data is from
-						<a href="https://console.firebase.google.com/project/uts-gas/database/uts-gas/data/~2Fusers"> firebase</a>
-					</p>
-					{this.state.users.map(user=><p>{user.username} {user.email}</p>)}
-				</header>
-			</div>
-		);
-	}
-
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Users get from backend api and the data is from
+            <a href="https://console.firebase.google.com/project/uts-gas/database/uts-gas/data/~2Fusers">
+              {" "}
+              firebase
+            </a>
+          </p>
+          {this.state.users.map((user) => (
+            <p>
+              {user.username} {user.email}
+            </p>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
