14a32bc3b2cd00e3c31aa69fa0210228c9d2efc0
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2020-08-24 23:42:16 +1000

    Updated Registration Page
    Updated registration page to match frontend design.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {getApi} from "./Api";
-import SignInSide from '../src/loginPage';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getApi } from "./Api";
+import SignInSide from "../src/loginPage";
+import RegisterSide from "../src/Register";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 class App extends React.Component {
-    state = {
-        users: [],
-    }
+  state = {
+    users: [],
+  };
 
-    componentDidMount() {
-        getApi('/users').then(data => this.setState({users: data}));
-    }
+  componentDidMount() {
+    getApi("/users").then((data) => this.setState({ users: data }));
+  }
 
-    render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<p>
-						Users get from backend api and the data is from
-						<a href="https://console.firebase.google.com/project/uts-gas/database/uts-gas/data/~2Fusers"> firebase</a>
-					</p>
-					{this.state.users.map(user=><p>{user.username} {user.email}</p>)}
-				</header>
-				<BrowserRouter>
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Users get from backend api and the data is from
+            <a href="https://console.firebase.google.com/project/uts-gas/database/uts-gas/data/~2Fusers">
+              {" "}
+              firebase
+            </a>
+          </p>
+          {this.state.users.map((user) => (
+            <p>
+              {user.username} {user.email}
+            </p>
+          ))}
+        </header>
+        <BrowserRouter>
           <Switch>
             {/* <Route exact path="/" component={Home} /> */}
             <Route path="/loginpage" component={SignInSide} />
+            <Route path="/register" component={RegisterSide} />
           </Switch>
-      </BrowserRouter>
-			</div>
-			
-		);
-	}
-
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
