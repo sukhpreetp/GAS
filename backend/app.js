63fe7355ac19f6cdbf7724b52c5e5a7fb3bbcd28
Krystian Huang <krystianhuang@gmail.com>
2020-08-26 21:32:29 +1000

    Change topics to skills

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var topicsRouter = require('./routes/topics');
+var skillsRouter = require('./routes/skills');
 var loginRouter = require('./routes/login');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/topics', topicsRouter);
+app.use('/skills', skillsRouter);
 app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
---
+++ backend/routes/skills.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../database');
+/* GET Topics listing. */
+router.get('/', function (req, res, next) {
+	const skillRef = db.ref('skills/');
+	skillRef.once('value')
+		.then(data=> {
+			data = data.val();
+			res.send(data);
+		});
+});
+
+/* ADD New Topic */
+router.post('/', function (req, res, next) {
+	const skillRef = db.ref('skills/');
+	skillRef.once('value')
+		.then(currentSkills=>{
+			const skills = currentSkills.val();
+			if(!skills.includes(req.body.skill)){
+				skills.push(req.body.skill);
+			}
+			skillRef.set(skills);
+			res.send(skills);
+		});
+});
+
+router.delete('/', function (req, res, next) {
+	const skillRef = db.ref('skills/');
+	skillRef.once('value')
+		.then(currentSkills=>{
+			let skills = currentSkills.val();
+			if(skills.includes(req.body.skill)){
+				skills = skills.filter(topic=> topic !== req.body.skill);
+			}
+			skillRef.set(skills);
+			res.send(skills);
+		});
+});
+
+
+module.exports = router;
+
